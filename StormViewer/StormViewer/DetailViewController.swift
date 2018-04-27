ffdf7bf6ece7364cf1a179fd617ab24382bf46fa
joseph park <jpark041@gmail.com>
2018-04-27 15:15:48 -0700

    deatilVC view linked with outlet

--- StormViewer/StormViewer/DetailViewController.swift
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
