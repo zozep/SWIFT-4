b461ed8ddc419a2d92310afc52e3ff66f9f055f8
joseph park <jpark041@gmail.com>
2018-05-03 20:08:56 -0700

    imageview created with IBOutlet imageview need to load images

---
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Joseph Park on 5/3/18.
+//  Copyright © 2018 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
