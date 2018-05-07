c8c7844f448607c0c51e8dbeffdcd6cdc899752b
joseph park <jpark041@gmail.com>
2018-05-06 19:32:51 -0700

    flexible spaced made with refresh button

--- WKWebView/WKWebView/ViewController.swift
+++ WKWebView/WKWebView/ViewController.swift
@@ -13,13 +13,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
     
-    
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +26,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.allowsBackForwardNavigationGestures = true
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
 
     }
 
