b6098796b9ca2c73c3f80bbb812f1e5d1ebc3abb
joseph park <jpark041@gmail.com>
2018-05-07 22:00:53 -0700

    words loaded to array and shuffles

--- WordScramble/WordScramble/ViewController.swift
+++ WordScramble/WordScramble/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UITableViewController {
 
-    
+    var allWords = [String]()
+    var usedWords = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+       
+        startGame()
+
+    }
+    
+    func startGame() {
+        //shuffles array
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        
+        //sets our view controller's title to be the first word in the array, once it has been shuffled
+        title = allWords[0]
+        
+        usedWords.removeAll(keepingCapacity: true)
+        
+        tableView.reloadData()
     }
 
     
