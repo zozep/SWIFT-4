aa4115beea133b0729ecbdd3758c62112b4af670
joseph park <jpark041@gmail.com>
2018-05-07 22:03:07 -0700

    tableView methods

--- WordScramble/WordScramble/ViewController.swift
+++ WordScramble/WordScramble/ViewController.swift
@@ -41,7 +41,15 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 
 
 }
