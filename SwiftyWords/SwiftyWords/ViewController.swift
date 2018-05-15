dfbd0bb0c613d37babcc8c569de120ba8cc77530
joseph park <jpark041@gmail.com>
2018-05-14 21:05:03 -0700

    IBoutlets & actions linked

--- SwiftyWords/SwiftyWords/ViewController.swift
+++ SwiftyWords/SwiftyWords/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cluesLabel: UILabel!
+    @IBOutlet var answersLabel: UILabel!
+    @IBOutlet var currentAnswer: UITextField!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    var letterButtons =  [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func letterTapped(btn: UIButton) {
+        //letterTapped
+        print("lettertapped()!")
     }
-
-
+    
+    @IBAction func submitTapped(_ sender: AnyObject) {
+        //submitTapped
+        print("submitTapped()!")
+    }
+    
+    @IBAction func clearTapped(_ sender: AnyObject) {
+        //clearTapped
+        print("clearTapped()!")
+    }
+    
 }
 
