7cb6c5c46e5cabc3a344af69b35bac0ab14d131d
joseph park <jpark041@gmail.com>
2018-05-16 17:35:59 -0700

    ARC

--- GCDPractice/GCDPractice/ViewController.swift
+++ GCDPractice/GCDPractice/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var reference1: Person?
+    var reference2: Person?
+    var reference3: Person?
+    var john: Person?
+    var unit4A: Apartment?
+    
+    class Person {
+        let name: String
+        init(name: String) { self.name = name }
+        var apartment: Apartment?
+        deinit { print("\(name) is being deinitialized") }
+    }
+    
+    class Apartment {
+        let unit: String
+        init(unit: String) { self.unit = unit }
+        var tenant: Person?
+        deinit { print("Apartment \(unit) is being deinitialized") }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        john = Person(name: "John Appleseed")
+        unit4A = Apartment(unit: "4A")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
