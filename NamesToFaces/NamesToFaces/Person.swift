c05b07ef8a9438505ce46235a7dd493479bf21f4
joseph park <jpark041@gmail.com>
2018-05-16 21:13:37 -0700

    Person Class made with properties & init

---
+++ NamesToFaces/NamesToFaces/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Joseph Park on 5/16/18.
+//  Copyright © 2018 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    //String! and String? can both be nil, but plain old String can't – it must have a value
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
