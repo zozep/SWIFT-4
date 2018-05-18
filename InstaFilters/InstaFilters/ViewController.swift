ca49d1c7d2525c87b57fcc1c5381d85806e18e03
joseph park <jpark041@gmail.com>
2018-05-17 18:10:58 -0700

    initial commit

---
+++ InstaFilters/InstaFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaFilters
+//
+//  Created by Joseph Park on 5/17/18.
+//  Copyright © 2018 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
