03b00209c092a55ba63b36a9b673093491614ba3
joseph park <jpark041@gmail.com>
2018-05-20 23:11:03 -0700

    save fx wip

--- InstaFilters/InstaFilters/ViewController.swift
+++ InstaFilters/InstaFilters/ViewController.swift
@@ -7,39 +7,40 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var intensity: UIButton!
-    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
     var currentImage: UIImage!
+    
+    var context: CIContext!
+    var currentFilter: CIFilter!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "YACIFP"
+        title = "insta filters"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        currentFilter = CIFilter(name: "CISepiaTone")
         
     }
 
-
-    @IBAction func changeFilter() {
-        //code
-        print("changeFilter()")
-    }
-
-    
-    @IBAction func save(_ sender: Any) {
-        //code
-        print("save()")
-    }
-    
-    @IBAction func intensityChanged(_ sender: Any) {
-        //code
-        print("intensityCHanged()")
+    // MARK: - ImagePicker fx
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else {
+            return
+        }
+        
+        dismiss(animated: true)
+        currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
     }
     
     @objc func importPicture() {
@@ -48,14 +49,63 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         picker.delegate = self
         present(picker, animated: true)
     }
-    
-    // MARK: - ImagePicker fx
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else {
-            return
-        }
-        dismiss(animated: true)
-        currentImage = image
+
+    @IBAction func changeFilter(_ sender: Any) {
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
     }
+    
+    func setFilter(action: UIAlertAction) {
+        // make sure we have a valid image before continuing!
+        guard currentImage != nil else { return }
+        
+        currentFilter = CIFilter(name: action.title!)
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
+        }
+        
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            self.imageView.image = processedImage
+        }
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        //code
+        print("save()")
+    }
+
     
 }
