f0d31a79e48c16ebf59ec715eade575e26870cbf
joseph park <jpark041@gmail.com>
2018-05-26 00:02:38 -0700

    completion closure & trailing closure syntax and explanation for animate(with:Duration)

--- CoreAnimation/CoreAnimation/ViewController.swift
+++ CoreAnimation/CoreAnimation/ViewController.swift
@@ -24,6 +24,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapped(_ sender: Any) {
+        //button is hideen so animations don't collide. it gets unhidden in the completion closure of the animation
+        tap.isHidden = true
+        
+        //We call animate(withDuration:) with a duration of 1 second, no delay, and no interesting options
+        UIView.animate(withDuration: 1, delay: 0, options: [],
+                       //For the animations closure we first do the usual [unowned self] in dance to avoid strong reference cycles, then enter the switch/case code.
+                       animations: { [unowned self] in
+                        //We switch using the value of self.currentAnimation. 'self' to make the closure capture clear
+                        switch self.currentAnimation {
+                        case 0:
+                            break
+                            
+                        default:
+                            break
+                        }
+                        //We use trailing closure syntax to provide our completion closure. This will be called when the animation completes, and its finished value will be true if the animations completed fully.
+        }) { [unowned self] (finished: Bool) in
+            //the completion closure unhides the tap button so it can be tapped again.
+            self.tap.isHidden = false
+        }
+        //After the animate(withDuration:) call, we have the old code to modify and wrap currentAnimation
         currentAnimation += 1
         
         if currentAnimation > 7 {
