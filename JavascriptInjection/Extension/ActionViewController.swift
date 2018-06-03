11019799b6b1d293feaf60877313cdf5de7bf98a
joseph park <jpark041@gmail.com>
2018-06-02 23:23:29 -0700

    adjustForKeyboard

--- JavascriptInjection/Extension/ActionViewController.swift
+++ JavascriptInjection/Extension/ActionViewController.swift
@@ -48,9 +48,31 @@ class ActionViewController: UIViewController {
                         self.title = self.pageTitle
                     }
                     
+                    let notificationCenter = NotificationCenter.default
+                    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
+                    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
                 }
             }
         }
+        
+        @objc func adjustForKeyboard(notification: Notification) {
+            let userInfo = notification.userInfo!
+            
+            let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+            let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+            
+            if notification.name == Notification.Name.UIKeyboardWillHide {
+                script.contentInset = UIEdgeInsets.zero
+            } else {
+                script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+            }
+            
+            script.scrollIndicatorInsets = script.contentInset
+            
+            let selectedRange = script.selectedRange
+            script.scrollRangeToVisible(selectedRange)
+        }
+        
         
     }
 
