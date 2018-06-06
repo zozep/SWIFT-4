8c113230e6c9ac1263bb6dc35f060ba2ade8c57d
joseph park <jpark041@gmail.com>
2018-06-05 23:22:41 -0700

    createScore() & createLives()

--- SlicePenguin/SlicePenguin/GameScene.swift
+++ SlicePenguin/SlicePenguin/GameScene.swift
@@ -14,6 +14,17 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
         background.position = CGPoint(x: 512, y: 384)
@@ -30,11 +41,27 @@ class GameScene: SKScene {
     }
     
     func createScore() {
-        print("creatScore()")
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        
+        addChild(gameScore)
+        
+        gameScore.position = CGPoint(x: 8, y: 8)
     }
+    
     func createLives() {
-        print("createLives()")
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+            
+            livesImages.append(spriteNode)
+        }
     }
+        
+        
     func createSlices() {
         print("createSlices()")
     }
