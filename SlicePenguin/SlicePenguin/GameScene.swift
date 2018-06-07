9bb21e994e67cd87787bf41b068b7daf957096c1
joseph park <jpark041@gmail.com>
2018-06-07 07:57:00 -0700

    screen set, touchesMoved, ended, touchesBegan, touchesEnded/Cancelled, redrawActiveSlice

--- SlicePenguin/SlicePenguin/GameScene.swift
+++ SlicePenguin/SlicePenguin/GameScene.swift
@@ -20,12 +20,13 @@ class GameScene: SKScene {
             gameScore.text = "Score: \(score)"
         }
     }
-    
+
     var livesImages = [SKSpriteNode]()
     var lives = 3
-    
     var activeSliceBG: SKShapeNode!
     var activeSliceFG: SKShapeNode!
+    var activeSlicePoints = [CGPoint]()
+
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
@@ -62,7 +63,10 @@ class GameScene: SKScene {
             livesImages.append(spriteNode)
         }
     }
-        
+    
+    //Track all player moves on the screen, recording an array of all their swipe points.
+    //Draw two slice shapes, one in white and one in yellow to make it look like there's a hot glow.
+    //User zPosition to make sure the slices go above everything else in the game
     func createSlices() {
         activeSliceBG = SKShapeNode()
         activeSliceBG.zPosition = 2
@@ -80,35 +84,73 @@ class GameScene: SKScene {
         addChild(activeSliceFG)
     }
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
+        super.touchesBegan(touches, with: event)
+        
+        //Remove all existing points in the activeSlicePoints array, because we're starting fresh
+        activeSlicePoints.removeAll(keepingCapacity: true)
+        
+        //Get the touch location and add it to the activeSlicePoints array.
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            activeSlicePoints.append(location)
+            
+            //Call the (as yet unwritten) redrawActiveSlice() method to clear the slice shapes
+            redrawActiveSlice()
+            
+            // Remove any actions that are currently attached to the slice shapes.
+            //This will be important if they are in the middle of a fadeOut(withDuration:) action.
+            activeSliceBG.removeAllActions()
+            activeSliceFG.removeAllActions()
+            
+            //Set both slice shapes to have an alpha value of 1 so they are fully visible.
+            activeSliceBG.alpha = 1
+            activeSliceFG.alpha = 1
+        }
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let location = touch.location(in: self)
+        
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        activeSliceBG.run(SKAction.fadeOut(withDuration: 0.25))
+        activeSliceFG.run(SKAction.fadeOut(withDuration: 0.25))
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesEnded(touches, with: event)
+    }
+
+    func redrawActiveSlice() {
+        //If we have fewer than two points in our array, we don't have enough data to draw a line so it needs to clear the shapes and exit the method
+        if activeSlicePoints.count < 2 {
+            activeSliceBG.path = nil
+            activeSliceFG.path = nil
+            return
+        }
+        
+        //If we have more than 12 slice points in our array, we need to remove the oldest ones until we have at most 12 – this stops the swipe shapes from becoming too long
+        while activeSlicePoints.count > 12 {
+            activeSlicePoints.remove(at: 0)
+        }
+        
+        //It needs to start its line at the position of the first swipe point, then go through each of the others drawing lines to each point
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+        
+        for i in 1 ..< activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+        
+        //Finally, it needs to update the slice shape paths so they get drawn using their designs – i.e., line width and color
+        activeSliceBG.path = path.cgPath
+        activeSliceFG.path = path.cgPath
+    }
+    
 }
