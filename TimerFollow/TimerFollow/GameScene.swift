242b42af42186cd1d296d195f0bda3462fac0487
joseph park <jpark041@gmail.com>
2018-06-08 09:17:51 -0700

    properties added, background set, lanchFireworks() wip

--- TimerFollow/TimerFollow/GameScene.swift
+++ TimerFollow/TimerFollow/GameScene.swift
@@ -6,15 +6,39 @@
 //  Copyright © 2018 Joseph Park. All rights reserved.
 //
 
-import SpriteKit
 import GameplayKit
+import SpriteKit
 
 class GameScene: SKScene {
+    var gameTimer: Timer!
+    var fireworks = [SKNode]()
+    
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    var score = 0 {
+        didSet {
+            // your code here
+        }
+    }
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
+    
+    @objc func launchFireworks() {
+        //createFirework()
+    }
 
+  
 }
