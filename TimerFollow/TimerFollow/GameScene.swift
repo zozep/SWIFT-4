bd94fe118caa0a89ea9f854746a928ac31a75301
joseph park <jpark041@gmail.com>
2018-06-08 09:22:45 -0700

    launchFireworks seprated with createFirework

--- TimerFollow/TimerFollow/GameScene.swift
+++ TimerFollow/TimerFollow/GameScene.swift
@@ -37,8 +37,89 @@ class GameScene: SKScene {
     }
     
     @objc func launchFireworks() {
-        //createFirework()
+        let movementAmount: CGFloat = 1800
+        
+        switch GKRandomSource.sharedRandom().nextInt(upperBound: 4) {
+        case 0:
+            // fire five, straight up
+            createFirework(xMovement: 0, x: 512, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 - 200, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 - 100, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 + 100, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 + 200, y: bottomEdge)
+            
+        case 1:
+            // fire five, in a fan
+            createFirework(xMovement: 0, x: 512, y: bottomEdge)
+            createFirework(xMovement: -200, x: 512 - 200, y: bottomEdge)
+            createFirework(xMovement: -100, x: 512 - 100, y: bottomEdge)
+            createFirework(xMovement: 100, x: 512 + 100, y: bottomEdge)
+            createFirework(xMovement: 200, x: 512 + 200, y: bottomEdge)
+            
+        case 2:
+            // fire five, from the left to the right
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 400)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 300)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 200)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 100)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge)
+            
+        case 3:
+            // fire five, from the right to the left
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 400)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 300)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 200)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 100)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge)
+            
+        default:
+            break
+        }
+    }
+    
+    func createFirework(xMovement: CGFloat, x: Int, y: Int) {
+        //Create an SKNode that will act as the firework container, and place it at the position that was specified.
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        // Create a rocket sprite node, give it the name "firework" so we know that it's the important thing, adjust its colorBlendFactor property so that we can color it, then add it to the container node.
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        // Give the firework sprite node one of three random colors: cyan, green or red. I've chosen cyan because pure blue isn't particularly visible on a starry sky background picture.
+        switch GKRandomSource.sharedRandom().nextInt(upperBound: 3) {
+        case 0:
+            firework.color = .cyan
+            
+        case 1:
+            firework.color = .green
+            
+        case 2:
+            firework.color = .red
+            
+        default:
+            break
+        }
+        
+        // Create a UIBezierPath that will represent the movement of the firework.
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+        
+        // Tell the container node to follow that path, turning itself as needed.
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        node.run(move)
+        
+        // Create particles behind the rocket to make it look like the fireworks are lit.
+        let emitter = SKEmitterNode(fileNamed: "fuse")!
+        emitter.position = CGPoint(x: 0, y: -22)
+        node.addChild(emitter)
+        
+        // Add the firework to our fireworks array and also to the scene.
+        fireworks.append(node)
+        addChild(node)
     }
 
-  
 }
