03031c656d6778080e6387858474a03c0b38be31
joseph park <jpark041@gmail.com>
2018-06-08 08:44:40 -0700

    fiewFor delegate method

--- MapKitPractice/MapKitPractice/ViewController.swift
+++ MapKitPractice/MapKitPractice/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
@@ -31,6 +31,35 @@ class ViewController: UIViewController {
 //        mapView.addAnnotation(washington)
         mapView.addAnnotations([london, oslo, paris, rome, washington])
 
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Define a reuse identifier. This is a string that will be used to ensure we reuse annotation views as much as possible
+        let identifier = "Capital"
+
+        // Check whether the annotation we're creating a view for is one of our Capital objects.
+        if annotation is Capital {
+            // Try to dequeue an annotation view from the map view's pool of unused views.
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            
+            if annotationView == nil {
+                // If it isn't able to find a reusable view, create a new one using MKPinAnnotationView and sets its canShowCallout property to true. This triggers the popup with the city name.
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView!.canShowCallout = true
+                
+                // Create a new UIButton using the built-in .detailDisclosure type. This is a small blue "i" symbol with a circle around it.
+                let btn = UIButton(type: .detailDisclosure)
+                annotationView!.rightCalloutAccessoryView = btn
+            } else {
+                //If it can reuse a view, update that view to use a different annotation.
+                annotationView!.annotation = annotation
+            }
+            
+            return annotationView
+        }
+        
+        // If the annotation isn't from a capital city, it must return nil so iOS uses a default view.
+        return nil
     }
     
     
