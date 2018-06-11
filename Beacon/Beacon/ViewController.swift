663322d0d5264258413055b65f993ce7336cdd40
joseph park <jpark041@gmail.com>
2018-06-11 10:10:07 -0700

    import CoreLocation, set delegate to self, request whenInUse Auth

--- Beacon/Beacon/ViewController.swift
+++ Beacon/Beacon/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager : CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        view.backgroundColor = UIColor.gray
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
-
