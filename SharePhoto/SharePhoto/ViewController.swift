b4f9bcce02dc0699d3bb0206c9df7eb6274ea84a
joseph park <jpark041@gmail.com>
2018-06-14 22:40:11 -0700

    added Collection VC to be initial controller, insets set to 10, cell size 145x145

---
+++ SharePhoto/SharePhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharePhoto
+//
+//  Created by Joseph Park on 6/14/18.
+//  Copyright © 2018 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
