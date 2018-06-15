a6a17b0485d619a8c02093bd67db813d405d6534
joseph park <jpark041@gmail.com>
2018-06-14 22:46:23 -0700

    collectionview protocol

--- SharePhoto/SharePhoto/ViewController.swift
+++ SharePhoto/SharePhoto/ViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var images = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "selfie share"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        return cell
+    }
+    
+    func importPicture() {
+        print("importPicture()")
+    }
 }
 
