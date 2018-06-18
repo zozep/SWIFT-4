bbac40bd8e0d478dde99922d5977cef454841190
joseph park <jpark041@gmail.com>
2018-06-18 00:33:36 -0700

    SpriteKit methods

--- RollingBall/RollingBall/GameScene.swift
+++ RollingBall/RollingBall/GameScene.swift
@@ -20,8 +20,7 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
     var player: SKSpriteNode!
-
-    
+    var lastTouchPosition: CGPoint?
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background.jpg")
@@ -30,12 +29,43 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+
         loadLevel()
     }
     
+    //MARK: SpriteKit protocol
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            lastTouchPosition = location
+        }
     }
     
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            lastTouchPosition = location
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if let currentTouch = lastTouchPosition {
+            let diff = CGPoint(x: currentTouch.x - player.position.x, y: currentTouch.y - player.position.y)
+            physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+        }
+    }
+    
+    
+    //MARK: Custom Fx
     func loadLevel() {
         if let levelPath = Bundle.main.path(forResource: "level1", ofType: "txt") {
             if let levelString = try? String(contentsOfFile: levelPath) {
