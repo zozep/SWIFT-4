7af037709a121449bc69cfdb409c079bb5fa8a58
joseph park <jpark041@gmail.com>
2018-06-18 00:24:13 -0700

    CollisonTypes enum

--- RollingBall/RollingBall/GameScene.swift
+++ RollingBall/RollingBall/GameScene.swift
@@ -9,6 +9,15 @@
 import SpriteKit
 import GameplayKit
 
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
+
+
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
