45546eb2353f465aa26d714c717c421d62366846
joseph park <jpark041@gmail.com>
2018-06-17 20:24:44 -0700

    loadLevel()

--- RollingBall/RollingBall/GameScene.swift
+++ RollingBall/RollingBall/GameScene.swift
@@ -16,4 +16,28 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
+    
+    func loadLevel() {
+        if let levelPath = Bundle.main.path(forResource: "level1", ofType: "txt") {
+            if let levelString = try? String(contentsOfFile: levelPath) {
+                let lines = levelString.components(separatedBy: "\n")
+                
+                for (row, line) in lines.reversed().enumerated() {
+                    for (column, letter) in line.enumerated() {
+                        let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                        
+                        if letter == "x" {
+                            // load wall
+                        } else if letter == "v"  {
+                            // load vortex
+                        } else if letter == "s"  {
+                            // load star
+                        } else if letter == "f"  {
+                            // load finish
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
