3f1bbaf8b1ec60e0294b769508dbd219340699f6
joseph park <jpark041@gmail.com>
2018-06-24 14:22:07 -0700

    initial commit

---
+++ AZChat/AZChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AZChat
+//
+//  Created by Joseph Park on 6/24/18.
+//  Copyright © 2018 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
