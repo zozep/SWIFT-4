0f57ab3574d35cac92726a2594123f49e54e44e7
joseph park <jpark041@gmail.com>
2018-07-08 22:23:30 -0700

    authenticateTapped button needs actual func

--- DeviceKeyChainTouchID/DeviceKeyChainTouchID/ViewController.swift
+++ DeviceKeyChainTouchID/DeviceKeyChainTouchID/ViewController.swift
@@ -20,23 +20,26 @@ class ViewController: UIViewController {
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
     }
-
+    
     @objc func adjustForKeyboard(notification: Notification) {
         let userInfo = notification.userInfo!
-        let keyboardScreenEndFrame = (userInfo([UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-
+        
         if notification.name == Notification.Name.UIKeyboardWillHide {
             secret.contentInset = UIEdgeInsets.zero
         } else {
-            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame, right: 0)
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
         }
+        
         secret.scrollIndicatorInsets = secret.contentInset
         
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
-        
     }
-
+    @IBAction func authenticateTapped(_ sender: Any) {
+        print("authenticateTapped()")
+    }
 }
 
