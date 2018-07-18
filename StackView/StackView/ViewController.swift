46cfed926c3744f38862696f480d99bcdfdda6d4
joseph park <jpark041@gmail.com>
2018-07-17 19:39:11 -0700

    storyboard designed

--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
